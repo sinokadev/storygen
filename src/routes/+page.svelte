167eca25d978b857c3f754731a22c84babd04e1b
sinokadev <sinoka@sinoka.dev>
2026-08-19 20:35:03 +0900

    수정 가능

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,17 +1,38 @@
-
 <script>
+    let thumtext = $state('world');
 
+    let today = new Date();   
+    let year = today.getFullYear();
+    let month = today.getMonth() + 1;
+    let date = today.getDate();
+
+    thumtext = `${year}. ${month}. ${date}.`;
+
+    let textsize = $state('5rem');
+    let position = $state('center-center');
 </script>
 
 <main>
-    <div id="thumbnail">
-        <div>2026. 10. 06.</div>
+    <!-- class={position} 및 style:--textsize={textsize} 연결 -->
+    <div id="thumbnail" class={position} style:--textsize={textsize}>
+        <div id="thumtext">{thumtext}</div>
     </div>
-    <input type="text" placeholder="섬네일에 들어갈 텍스트를 입력하세요...">
+    
+    <input type="text" placeholder="섬네일에 들어갈 텍스트를 입력하세요..." bind:value={thumtext}>
 
     <p>텍스트 정렬</p>
     <div id="position-select">
-        <input type="checkbox"><input type="checkbox"><input type="checkbox"><input type="checkbox"><input type="checkbox"><input type="checkbox"><input type="checkbox"><input type="checkbox"><input type="checkbox">
+        <input type="radio" bind:group={position} value="top-left" />
+        <input type="radio" bind:group={position} value="top-center" />
+        <input type="radio" bind:group={position} value="top-right" />
+
+        <input type="radio" bind:group={position} value="center-left" />
+        <input type="radio" bind:group={position} value="center-center" />
+        <input type="radio" bind:group={position} value="center-right" />
+
+        <input type="radio" bind:group={position} value="bottom-left" />
+        <input type="radio" bind:group={position} value="bottom-center" />
+        <input type="radio" bind:group={position} value="bottom-right" />
     </div>
 </main>
 
@@ -21,17 +42,22 @@
         flex-direction: column !important;
         align-items: center !important;
         text-align: center !important;
+        gap: 10px;
+    }
+    #thumtext {
+        font-size: var(--textsize);
     }
     #thumbnail {
-        box-sizing: border-box; /* 패딩과 테두리를 width 내부로 포함 */
+        box-sizing: border-box;
         width: 100%;
         aspect-ratio: 16 / 9;
-        object-fit: cover;
         padding: 1em;
         border-radius: 5px;
-        border-style: solid;
-        border-width: 2px;
-        border-color: black;
+        border: 2px solid black;
+
+        display: flex;
+        justify-content: center;
+        align-items: center;
     }
     #position-select {
         display: grid;
@@ -40,4 +66,17 @@
         width: 100px;
         height: 100px;
     }
+
+    /* 위치 클래스들 */
+    #thumbnail.top-left       { justify-content: flex-start; align-items: flex-start; }
+    #thumbnail.top-center     { justify-content: center;     align-items: flex-start; }
+    #thumbnail.top-right      { justify-content: flex-end;   align-items: flex-start; }
+    
+    #thumbnail.center-left    { justify-content: flex-start; align-items: center; }
+    #thumbnail.center-center  { justify-content: center;     align-items: center; }
+    #thumbnail.center-right   { justify-content: flex-end;   align-items: center; }
+    
+    #thumbnail.bottom-left    { justify-content: flex-start; align-items: flex-end; }
+    #thumbnail.bottom-center  { justify-content: center;     align-items: flex-end; }
+    #thumbnail.bottom-right   { justify-content: flex-end;   align-items: flex-end; }
 </style>
